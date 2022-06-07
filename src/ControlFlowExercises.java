09b75e9ace09eb5c6615c80f9ea14f0fc840bbab
Jesse Mammosser <jesse.mamo123@gmail.com>
2022-06-07 16:54:47 -0400

    feat: end of day

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+    public static void main(String[] args){
+//        int i = 5;
+//        // check the value of the loop control variable
+//        while (i <= 15) {
+//            System.out.println("This is inside the loop. Before i is incremented though. The value of i is " + i);
+//            // modify the loop control variable
+//            i = i + 1;
+//            System.out.println("This is still inside the loop, but now the loop control variable has incremented. The value of i is now " + i);
+
+//            int i = 2;
+//        do {
+//            System.out.println("This is inside the loop. Before i is incremented though. The value of i is " + i);
+//            // modify the loop control variable
+//            i = i + 2;
+//            System.out.println("This is still inside the loop, but now the loop control variable has incremented. The value of i is now " + i);
+//        } while (i < 100);
+
+
+//        int i = 100;
+//        do {
+//            System.out.println("This is inside the loop. Before i is incremented though. The value of i is " + i);
+//            // modify the loop control variable
+//            i = i - 5;
+//            System.out.println("This is still inside the loop, but now the loop control variable has incremented. The value of i is now " + i);
+//        } while (i > -10);
+//    }
+//        }
+//        int i = 2;
+//        do {
+//            System.out.println("This is inside the loop. Before i is incremented though. The value of i is " + i);
+//            // modify the loop control variable
+//            i = i * i;
+//            System.out.println("This is still inside the loop, but now the loop control variable has incremented. The value of i is now " + i);
+//        } while (i < 65536);
+//    }
+//}
+//        int i;
+//        for (i = 5; i < 15; i++){
+//            System.out.println("oh hello " + i);
+//        }
+//        System.out.println("final product " + i);
+//
+//
+//    }
+//}
+//        int i;
+//        for (i = 2; i < 100; i+=2){
+//            System.out.println("oh hello " + i);
+//        }
+//        System.out.println("final product " + i);
+//        int i;
+//        for (i =100; i > -10; i-=2){
+//            System.out.println("oh hello " + i);
+//        }
+//        System.out.println("final product " + i);
+//        int i;
+//        for (i =2; i < 65536; i*=i){
+//            System.out.println("oh hello " + i);
+//        }
+//        System.out.println("final product " + i);
+        int i;
+        for(i = 0; i <= 100; i++)
+            if(i % 3 == 0 && i % 5 == 0)
+                System.out.println(i);
+    }
+    }
+

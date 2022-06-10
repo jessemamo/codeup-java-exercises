c6241becb788f0bd4a60cff0b2f11dc76b28fb7b
Jesse Mammosser <jesse.mamo123@gmail.com>
2022-06-10 16:51:29 -0400

    feat: mini exercises and oop

---
+++ src/Person.java
@@ -0,0 +1,51 @@
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello, " + name);
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person("Jesse");
+
+        System.out.println(person.getName());
+        person.sayHello();
+        person.setName("Jesse Mammosser");
+        System.out.println(person.getName());
+        person.sayHello();
+
+//      The following code blocks make use of the Person class from the previous exercise. For each code block, think about what the code will output, then copy the code into IntelliJ and run it to see if it matches your expectations.
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+//
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+
+
+    }
+
+}

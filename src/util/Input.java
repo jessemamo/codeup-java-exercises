28a741b7f970126595d09c633c8dc95e881ae490
Jesse Mammosser <jesse.mamo123@gmail.com>
2022-06-11 08:57:15 -0400

    feat: getString

--- src/util/Input.java
+++ src/util/Input.java
@@ -2,7 +2,20 @@ package util;
 
 import java.util.Scanner;
 
+
 public class Input {
     private Scanner scanner;
 
+    public String getString() {
+        System.out.println("Please enter a string");
+        String userInput = scanner.nextLine();
+        return userInput;
+    }
+
+    public Input() {
+        this.scanner = new Scanner(System.in);
+
+
+
+    }
 }
